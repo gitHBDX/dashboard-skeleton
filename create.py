fe9f72d6b030638891864521787203a17f6c49eb
Maurice Frank <mfrank@hb-dx.com>
2023-11-21 12:58:32 +0100

    test

--- create.py
+++ create.py
@@ -1,1 +1,11 @@
-print("Create a new file")
+import os
+
+print_red = lambda x: print("\033[31m{}\033[0m".format(x))
+print_green = lambda x: print("\033[32m{}\033[0m".format(x))
+print_yellow = lambda x: print("\033[33m{}\033[0m".format(x))
+
+if os.listdir(".") != []:
+    print_red("Directory is not empty")
+    exit(1)
+
+
