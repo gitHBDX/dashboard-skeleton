559a356d6e8aafc44c06f246d461b6459b077db1
Maurice Frank <mfrank@hb-dx.com>
2023-12-04 12:43:02 +0100

    test

--- skeleton/src/pages/example.py
+++ skeleton/src/pages/example.py
@@ -3,37 +3,50 @@ from dash import html
 import dash_mantine_components as dmc
 
 
-layout =   html.Div([     dmc.Stack([
-                    dmc.Paper([
+layout = html.Group(
+    [
+        dmc.Stack(
+            [
+                dmc.Paper(
+                    [
                         dmc.Title("Hello World", order=1),
                         dmc.Text("This is a text", size="xs"),
-                    ]),
-
-
-
-
-                    dmc.Paper([
-                        dmc.Stack([
-                            dmc.Text("The most useful elements are dmc.Stack, dmc.Group and dmc.Paper", size="xs"),
-                            dmc.Text("dmc.Stack is a vertical stack of elements", size="xs", c="blue"),
-                            dmc.Text("dmc.Group is a horizontal stack of elements", size="xs", c="green"),
-                            dmc.Text("dmc.Paper is a box with a shadow", size="xs", c="dimmed"),
-                        ])
-                    ]),
-                ]),
-                dmc.Stack([
-                    dmc.Paper([
+                    ]
+                ),
+                dmc.Paper(
+                    [
+                        dmc.Stack(
+                            [
+                                dmc.Text("The most useful elements are dmc.Stack, dmc.Group and dmc.Paper", size="xs"),
+                                dmc.Text("dmc.Stack is a vertical stack of elements", size="xs", c="blue"),
+                                dmc.Text("dmc.Group is a horizontal stack of elements", size="xs", c="green"),
+                                dmc.Text("dmc.Paper is a box with a shadow", size="xs", c="dimmed"),
+                            ]
+                        )
+                    ]
+                ),
+            ]
+        ),
+        dmc.Stack(
+            [
+                dmc.Paper(
+                    [
                         dmc.TextInput("This is a text input", label="Easy inputs", description="This is a description"),
-                         dmc.RadioGroup(
-                            [dmc.Radio(l, value=k) for k, l in [["react", "React"], ["ng", "Angular"], ["svelte", "Svelte"], ["vue", "Vue"]]],
+                        dmc.RadioGroup(
+                            [
+                                dmc.Radio(l, value=k)
+                                for k, l in [["react", "React"], ["ng", "Angular"], ["svelte", "Svelte"], ["vue", "Vue"]]
+                            ],
                             id="radiogroup-simple",
                             value="react",
                             label="Select your favorite framework/library",
                             size="sm",
                             mt=10,
                         ),
-                    ]),
-                    dmc.Group([
+                    ]
+                ),
+                dmc.Group(
+                    [
                         dmc.Card(
                             children=[
                                 dmc.CardSection(
@@ -104,9 +117,12 @@ layout =   html.Div([     dmc.Stack([
                             shadow="sm",
                             radius="md",
                         ),
-                    ])
-                ])
-            ])
+                    ]
+                ),
+            ]
+        ),
+    ]
+)
 
 
-dash.register_page("example", title="Example - {app_name}")
+dash.register_page("example", layout=layout, path="/", title="Example - {app_name}")
